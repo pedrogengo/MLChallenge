943738a36c7585b8fc58126d525943fadee3be87
Pedro Gengo <pedro.gabriel.lourenco@hotmail.com>
2021-07-03 16:10:57 -0300

    FEATURE: Add lambda entrypoint to use as backend for api gateway

--- app/entrypoint/lambda_entrypoint.py
+++ app/entrypoint/lambda_entrypoint.py
@@ -1,26 +1,20 @@
 import boto3
 import logging
-import os
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-QUEUE_NAME = os.getenv("QUEUE_NAME")
-SQS = boto3.client("sqs")
 
 def handler(event, context):
+    lambda_client = boto3.client('lambda')
 
-    data = event['body']
+    link = event['pathParameters']['link']
+    msg = {"link": link}
 
-    queue_url = SQS.get_queue_url(QueueName=QUEUE_NAME).get('QueueUrl')
-    logger.debug("Queue URL is %s", queue_url)
-
-    logger.debug("Recording with event %s", event)
-    try:
-        logger.debug("Recording %s", data)
-        resp = SQS.send_message(QueueUrl=queue_url, MessageBody=data)
-        logger.debug("Send result: %s", resp)
-        return 'ok'
-
-    except Exception as e:
-        raise Exception("Could not record link! %s" % e)
+    invoke_response = lambda_client.invoke(FunctionName="feature_generation",
+                                           InvocationType='Event',
+                                           Payload=json.dumps(msg))
+    print(invoke_response)
+    if invoke_response['statusCode'] == 200:
+        return invoke_response
