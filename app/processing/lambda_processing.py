e5497fe296e0f2bfe554be0741be09ed8bc712f6
Pedro Gengo <pedro.gabriel.lourenco@hotmail.com>
2021-07-04 13:24:25 -0300

    FIX: Change dynamo scan call in lambda processing

--- app/processing/lambda_processing.py
+++ app/processing/lambda_processing.py
@@ -55,7 +55,7 @@ class Crawler:
             logging.exception(f'Failed to crawl: {url}')
 
 def get_visited_urls(client, tablename):
-    response = client.Table(TableName=tablename)
+    response = client.scan(TableName=tablename)
     data = response['Items']
     while 'LastEvaluatedKey' in response:
         response = client.scan(TableName=tablename,
